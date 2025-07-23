59a5256768d165049fde64763461fc589b475900
dina <20Ahmed.Ammar10@gmail.com>
2025-07-23 20:03:16 +0300

    edit style

--- src/features/menu/menu.jsx
+++ src/features/menu/menu.jsx
@@ -1,7 +1,7 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PageHeader from "../../reusableComponents/pageHeader";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Menu() {
   const [categories, setCategories] = useState([]);
@@ -24,14 +24,11 @@ export default function Menu() {
         const response = await axios.get(
           "https://restaurantapi-production-f574.up.railway.app/api/categories"
         );
-        console.log("Fetched categories:", response.data);
-          setCategories(response.data.data);
-          setError("");
-        
+        setCategories(response.data.data);
+        setError("");
       } catch (err) {
         console.error("خطأ في جلب التصنيفات", err);
         setError("حدث خطأ أثناء تحميل التصنيفات.");
-
       } finally {
         setLoading(false);
       }
@@ -46,16 +43,11 @@ export default function Menu() {
     async function fetchMeals() {
       try {
         const categoryId = categories[activeIndex]?.id;
-        console.log("Fetching meals for category ID:", categoryId);
-
         const response = await axios.get(
           `https://restaurantapi-production-f574.up.railway.app/api/categories/${categoryId}/meals`
         );
-
-        console.log("Fetched meals:", response.data);
-        setMeals(response.data.meals|| []);
+        setMeals(response.data.meals || []);
         setError("");
-
       } catch (error) {
         console.error("خطأ في جلب الأصناف", error);
         setMeals([]);
@@ -88,15 +80,15 @@ export default function Menu() {
     return <p className="text-center text-red-600 mt-10">{error}</p>;
   }
 
-  const prevIndex =
-    (activeIndex - 1 + categories.length) % categories.length;
+  const prevIndex = (activeIndex - 1 + categories.length) % categories.length;
   const nextIndex = (activeIndex + 1) % categories.length;
 
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <PageHeader title="المنيو" subtitle="تمتع بافضل الاصناف " />
 
-      <div className="flex items-center justify-center gap-0.5 sm:gap-40 mb-6 mt-16">
+      {/* التصنيفات */}
+      <div className="flex items-center justify-center gap-0.5 sm:gap-40 my-16">
         <button
           onClick={prev}
           className="text-lg font-bold p-1 md:p-2 rounded-full md:bg-pink-100 md:hover:bg-pink-200"
@@ -135,29 +127,36 @@ export default function Menu() {
         </button>
       </div>
 
-      <div className="flex justify-center items-center p-4 bg-white mt-20 flex-wrap gap-6">
+    
+      <div className="flex flex-wrap justify-center gap-4 px-4 pb-8 bg-white mt-20 min-h-[150px]">
+        
         {meals.length > 0 ? (
-          meals.map((meal) => (
-            <div
+          <AnimatePresence >
+            {meals.map((meal) => (
+              <motion.div
               key={meal.id}
-              className="group relative rounded-lg p-4 flex flex-col justify-around mx-auto h-30 w-40 cursor-pointer bg-white text-pink-800 overflow-hidden shadow hover:shadow-lg transition-shadow duration-400"
-            >
-              <div className="absolute inset-0 bg-[url('/menu.jpg')] bg-cover bg-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 z-0" />
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: -100, opacity: 0 }}
+              transition={{ duration: 0.4 }}
+              className="group relative rounded-lg p-2 flex flex-col justify-around w-[160px] h-[120px] bg-white text-pink-800 overflow-hidden shadow hover:shadow-lg"
 
-              <h3 className="text-lg font-semibold relative z-10 mb-1">
+              >
+            <div className="absolute inset-0 bg-[url('/menu.jpg')] bg-cover bg-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 z-0" />
+              <h3 className="text-base font-semibold relative z-10 text-center">
                 {meal.name}
               </h3>
-              <p className="relative text-lg font-bold z-10">
-                {meal.price} جنيه
+              <p className="text-sm font-bold relative z-10 text-center">
+                  {meal.price} جنيه
               </p>
-            </div>
-          ))
+              </motion.div>
+          ))}
+          </AnimatePresence>
         ) : (
-          <p className="col-span-full text-center text-gray-500">
-            لا توجد وجبات لعرضها
-          </p>
+        <p className="text-center text-gray-500 w-full">لا توجد وجبات لعرضها</p>
         )}
-      </div>
+        </div>
+
     </div>
   );
 }
